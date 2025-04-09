4b1002f9a76a4c4365297495e1956c48953fde7a
Woomin-Wang <wwmin828@gmail.com>
2025-04-10 00:03:50 +0900

    1

--- src/inflearn_java_middle/nested/local/LocalOuterV4.java
+++ src/inflearn_java_middle/nested/local/LocalOuterV4.java
@@ -12,13 +12,21 @@ public class LocalOuterV4 {
             @Override
             public void print() {
                 System.out.println("value = " + value);
+
+                //인스턴스는 지역 변수보다 더 오래 살아남는다.
                 System.out.println("localVar = " + localVar);
                 System.out.println("paramVar = " + paramVar);
+
                 System.out.println("outInstanceVar = " + outInstanceVar);
             }
         }
 
-        return new LocalPrinter();
+        Printer printer = new LocalPrinter();
+        // 만약 localVar의 값을 변경한다면? 다시 캡쳐?
+        // localVar = 10;
+        // paramVar = 20;
+
+        return printer;
     }
 
     public static void main(String[] args) {
