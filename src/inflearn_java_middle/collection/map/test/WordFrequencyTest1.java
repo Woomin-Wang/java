ebc078f8a40a233555c147cdcd25a088281be24f
Woomin-Wang <wwmin828@gmail.com>
2025-04-29 17:11:39 +0900

    map

---
+++ src/inflearn_java_middle/collection/map/test/WordFrequencyTest1.java
@@ -0,0 +1,11 @@
+package inflearn_java_middle.collection.map.test;
+
+import java.util.HashMap;
+
+public class WordFrequencyTest1 {
+    public static void main(String[] args) {
+        String text = "orange banana apple apple banana apple";
+
+        HashMap<String, Integer> objectObjectHashMap = new HashMap<>();
+    }
+}
