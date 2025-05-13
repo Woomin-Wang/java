9134a3691688479259b764abd5c21c2fd218daa5
Woomin-Wang <wwmin828@gmail.com>
2025-05-13 13:54:07 +0900

    code: Exception

--- src/this_is_java/ch09/sec06/exam3/Button.java
+++ src/this_is_java/ch09/sec06/exam3/Button.java
@@ -3,7 +3,7 @@ package this_is_java.ch09.sec06.exam3;
 import this_is_java.ch06.sec13.exam02.package1.C;
 
 public class Button {
-    // 정적 멤머 인터페이스
+    // 정적 멤버 인터페이스
     public static interface ClickListener {
         // 추상 메서드
         void onClick();
---
+++ src/this_is_java/ch11/sec06/AccountExample.java
@@ -0,0 +1,16 @@
+package this_is_java.ch11.sec06;
+
+public class AccountExample {
+    public static void main(String[] args) {
+        Account account = new Account();
+
+        account.deposit(10000);
+        System.out.println("예금액: "  + account.getBalance());
+
+        try {
+            account.withdraw(30000 );
+        } catch (InsufficientException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
